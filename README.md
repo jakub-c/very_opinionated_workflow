3bb7e1b7decb19d87dd1a1a5f8b01ec507824754
Jakub <jc@kunde.dk>
2015-06-12 15:18:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Very opinionated workflow
+**for static HTML/SASS/JS projects**
+
+This project is meant to simplify the repetitive process of creating static web projects. It's based on subjective choice of tools and frameworks (gulp, bootstrap, html5 boilerplate, sass etc.). Cut down to bare essentials.
+
+##How to:
+`gulp` - starts dev mode
+
+`gulp dist` - minifies code, optimizes images
+
+Images should be kept in `app/img` folder
+
+Styleguide provided by JSCS
---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps   = require('gulp-sourcemaps');
+var del          = require('del');
+var runSequence  = require('run-sequence');
+var minifyCss    = require('gulp-minify-css');
+var imagemin     = require('gulp-imagemin');
+var uglify       = require('gulp-uglify');
+var jscs         = require('gulp-jscs');
+var notify       = require("gulp-notify");
+var plumber      = require('gulp-plumber');
+
+var notifyError = function(err, lang) {
+  console.log("------------------ error ------------------");
+  console.log(err.toString());
+  console.log("-------------------------------------------");
+  notify.onError({
+    title: lang + " error",
+    message: "Check console",
+    sound: "Basso"
+  })(err);
+};
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 9',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("app/sass/*.scss")
+    .pipe(plumber(function(){
+       this.emit('end');
+      }))
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .on("error", function(err) {
+      notifyError(err, "SASS")
+    })
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: AUTOPREFIXER_BROWSERS
+    }))
+    .pipe(sourcemaps.write()) 
+    .pipe(gulp.dest("app/"))
+    .pipe(browserSync.stream())
+    .pipe(plumber.stop());
+});
+gulp.task('sass-dist', function() {
+  return gulp.src("app/sass/*.scss")
+    .pipe(sass({
+      errLogToConsole: false
+    }))
+    .on("error", function(err) {
+      notifyError(err, "SASS")
+    })
+    .pipe(autoprefixer({
+      browsers: AUTOPREFIXER_BROWSERS
+    }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest("app/"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'lint'], function() {
+  browserSync.init({
+    server: "./app",
+    open: false,
+    ghostMode: false
+  });
+  gulp.watch("app/sass/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/js/**/*", ['lint']).on('change', browserSync.reload);
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['app/js/*.js'])
+    .pipe(plumber())
+    .pipe(jscs())
+    .on("error", function(err) {
+      notifyError(err, "JS Style")
+    })
+    .pipe(plumber.stop());
+});
+
+gulp.task('clean-dist', function(cb){
+  del('dist', cb);
+});
+gulp.task('copy-to-dist', function(){
+  return gulp.src(['app/**/*', '!app/sass{,/**}', '!app/img{,/**}', '!app/js{,/**}'], {base: "./app"})
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('copy-images', function () {
+  return gulp.src(['app/img/**/*'], {base: "./app"})
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/'));
+});
+gulp.task('copy-js', function () {
+  return gulp.src(['app/js/**/*'], {base: "./app"})
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['serve']);
+gulp.task('dist', function(callback) {
+  runSequence('lint', 'clean-dist', 'sass-dist', 'copy-to-dist', 'copy-images', 'copy-js');
+});
