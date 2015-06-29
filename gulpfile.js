946d0924359f0bf6bd6e26e66aa327713293ecd9
Jakub <jc@kunde.dk>
2015-06-29 16:16:45 +0200

    fixes syntax to be jscs compilant

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,17 @@ var minifyCss    = require('gulp-minify-css');
 var imagemin     = require('gulp-imagemin');
 var uglify       = require('gulp-uglify');
 var jscs         = require('gulp-jscs');
-var notify       = require("gulp-notify");
+var notify       = require('gulp-notify');
 var plumber      = require('gulp-plumber');
 
 var notifyError = function(err, lang) {
-  console.log("------------------ error ------------------");
+  console.log('------------------ error ------------------');
   console.log(err.toString());
-  console.log("-------------------------------------------");
+  console.log('-------------------------------------------');
   notify.onError({
-    title: lang + " error",
-    message: "Check console",
-    sound: "Basso"
+    title: lang + ' error',
+    message: 'Check console',
+    sound: 'Bass'
   })(err);
 };
 
@@ -37,82 +37,93 @@ var AUTOPREFIXER_BROWSERS = [
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("app/sass/*.scss")
-    .pipe(plumber(function(){
-       this.emit('end');
+  return gulp.src('app/sass/*.scss')
+    .pipe(plumber(function() {
+        this.emit('end');
       }))
     .pipe(sass({
       errLogToConsole: true
     }))
-    .on("error", function(err) {
-      notifyError(err, "SASS")
+    .on('error', function(err) {
+      notifyError(err, 'SASS')
     })
     .pipe(sourcemaps.init())
     .pipe(autoprefixer({
       browsers: AUTOPREFIXER_BROWSERS
     }))
-    .pipe(sourcemaps.write()) 
-    .pipe(gulp.dest("app/"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/'))
     .pipe(browserSync.stream())
     .pipe(plumber.stop());
 });
 gulp.task('sass-dist', function() {
-  return gulp.src("app/sass/*.scss")
+  return gulp.src('app/sass/*.scss')
     .pipe(sass({
       errLogToConsole: false
     }))
-    .on("error", function(err) {
-      notifyError(err, "SASS")
+    .on('error', function(err) {
+      notifyError(err, 'SASS')
     })
     .pipe(autoprefixer({
       browsers: AUTOPREFIXER_BROWSERS
     }))
     .pipe(minifyCss())
-    .pipe(gulp.dest("app/"))
+    .pipe(gulp.dest('app/'))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'lint'], function() {
   browserSync.init({
-    server: "./app",
+    server: './app',
     open: false,
     ghostMode: false
   });
-  gulp.watch("app/sass/*.scss", ['sass']);
-  gulp.watch("app/*.html").on('change', browserSync.reload);
-  gulp.watch("app/js/**/*", ['lint']).on('change', browserSync.reload);
+  gulp.watch('app/sass/*.scss', ['sass']);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch(['app/js/**/*', './gulpfile.js'], ['lint'])
+    .on('change', browserSync.reload);
 });
 
-gulp.task('lint', function () {
-  return gulp.src(['app/js/*.js'])
+gulp.task('lint', function() {
+  return gulp.src(['app/js/*.js', './gulpfile.js'])
     .pipe(plumber())
     .pipe(jscs())
-    .on("error", function(err) {
-      notifyError(err, "JS Style")
+    .on('error', function(err) {
+      notifyError(err, 'JS Style')
     })
     .pipe(plumber.stop());
 });
 
-gulp.task('clean-dist', function(cb){
+gulp.task('scripts', function() {
+
+});
+
+gulp.task('clean-dist', function(cb) {
   del('dist', cb);
 });
-gulp.task('copy-to-dist', function(){
-  return gulp.src(['app/**/*', '!app/sass{,/**}', '!app/img{,/**}', '!app/js{,/**}'], {base: "./app"})
+gulp.task('copy-to-dist', function() {
+  return gulp.src([
+      'app/**/*', '!app/sass{,/**}', '!app/img{,/**}', '!app/js{,/**}'
+    ],
+    {base: './app'})
     .pipe(gulp.dest('dist'));
 });
-gulp.task('copy-images', function () {
-  return gulp.src(['app/img/**/*'], {base: "./app"})
+gulp.task('copy-images', function() {
+  return gulp.src(['app/img/**/*'], {base: './app'})
     .pipe(imagemin())
     .pipe(gulp.dest('dist/'));
 });
-gulp.task('copy-js', function () {
-  return gulp.src(['app/js/**/*'], {base: "./app"})
+gulp.task('copy-js', function() {
+  return gulp.src(['app/js/**/*'], {base: './app'})
     .pipe(uglify())
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', ['serve']);
 gulp.task('dist', function(callback) {
-  runSequence('lint', 'clean-dist', 'sass-dist', 'copy-to-dist', 'copy-images', 'copy-js');
+  runSequence(
+    'lint', 'clean-dist', 'sass-dist',
+    'copy-to-dist', 'copy-images', 'copy-js'
+  );
 });
